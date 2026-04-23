f53024e8ac0f74bcf69393dc611a73613de7c680
Rodrigo-Riquelme-Saavedra <Rodrigo.Riquelme@gvsolution.cl>
2026-04-23 19:45:28 -0400

    Update jsonbin.js

--- js/jsonbin.js
+++ js/jsonbin.js
@@ -1,1 +1,29 @@
+const JSONBIN = {
+  BASE: "https://api.jsonbin.io/v3/b",
+  KEY: "TU_MASTER_KEY_AQUI",
+  BINS: {
+    usuarios:    "TU_BIN_ID_USUARIOS",
+    tramites:    "TU_BIN_ID_TRAMITES",
+    documentos:  "TU_BIN_ID_DOCUMENTOS"
+  },
 
+  async get(bin) {
+    const r = await fetch(`${this.BASE}/${this.BINS[bin]}/latest`, {
+      headers: { "X-Master-Key": this.KEY }
+    });
+    const data = await r.json();
+    return data.record;
+  },
+
+  async set(bin, record) {
+    const r = await fetch(`${this.BASE}/${this.BINS[bin]}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Master-Key": this.KEY
+      },
+      body: JSON.stringify(record)
+    });
+    return await r.json();
+  }
+};
